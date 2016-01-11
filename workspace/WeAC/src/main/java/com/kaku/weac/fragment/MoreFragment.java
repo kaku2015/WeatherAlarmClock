e90fd4ec6c4db45e88264d57889790172be96211
kaku2015 <kaku201313@163.com>
2016-01-11 20:52:17 +0800

    more1

--- workspace/WeAC/src/main/java/com/kaku/weac/fragment/MoreFragment.java
+++ workspace/WeAC/src/main/java/com/kaku/weac/fragment/MoreFragment.java
@@ -4,7 +4,6 @@
 package com.kaku.weac.fragment;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
@@ -13,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.kaku.weac.R;
 import com.kaku.weac.activities.ThemeActivity;
@@ -26,7 +24,7 @@ import com.kaku.weac.util.MyUtil;
  * @author 咖枯
  * @version 1.0 2015
  */
-public class MoreFragment extends BaseFragment {
+public class MoreFragment extends BaseFragment implements OnClickListener {
     /**
      * Log tag ：MoreFragment
      */
@@ -43,18 +41,9 @@ public class MoreFragment extends BaseFragment {
         LogUtil.i(LOG_TAG, "MoreFragmet:  onCreateView");
 
         View view = inflater.inflate(R.layout.fm_more, container, false);
-        // 变更主题按钮
-        Button button = (Button) view.findViewById(R.id.button);
-        button.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), ThemeActivity.class);
-                // 启动主题界面
-                startActivityForResult(intent, REQUEST_THEME_WALLPAPER);
-
-            }
-        });
+        // 变更主题
+        ViewGroup themeBtn = (ViewGroup) view.findViewById(R.id.theme);
+        themeBtn.setOnClickListener(this);
         return view;
     }
 
@@ -83,70 +72,14 @@ public class MoreFragment extends BaseFragment {
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        LogUtil.i(LOG_TAG, "MoreFragmet:  onActivityCreated");
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.theme:
+                Intent intent = new Intent(getActivity(), ThemeActivity.class);
+                // 启动主题界面
+                startActivityForResult(intent, REQUEST_THEME_WALLPAPER);
+                break;
+
+        }
     }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        LogUtil.i(LOG_TAG, "MoreFragmet:  onStop");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        LogUtil.i(LOG_TAG, "MoreFragmet:  onStart");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        LogUtil.i(LOG_TAG, "MoreFragmet:  onResume");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        LogUtil.i(LOG_TAG, "MoreFragmet:  onPause");
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        LogUtil.i(LOG_TAG, "MoreFragmet:  onDetach");
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        LogUtil.i(LOG_TAG, "MoreFragmet:  onAttach");
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        LogUtil.i(LOG_TAG, "MoreFragmet:  onCreate");
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        LogUtil.i(LOG_TAG, "MoreFragmet:  onViewCreated");
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        LogUtil.i(LOG_TAG, "MoreFragmet:  onDestroyView");
-
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        LogUtil.i(LOG_TAG, "MoreFragmet:  onDestroy");
-    }
-
 }

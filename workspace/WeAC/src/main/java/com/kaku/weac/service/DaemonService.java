389c6d05cefcf79ff71fc220c147bd88becfedd6
kaku2015 <kaku201313@163.com>
2016-06-08 21:06:58 +0800

    修改灰色保活

--- workspace/WeAC/src/main/java/com/kaku/weac/service/DaemonService.java
+++ workspace/WeAC/src/main/java/com/kaku/weac/service/DaemonService.java
@@ -3,12 +3,27 @@
  */
 package com.kaku.weac.service;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.IBinder;
+import android.os.SystemClock;
 
 import com.coolerfall.daemon.Daemon;
+import com.kaku.weac.bean.AlarmClock;
+import com.kaku.weac.broadcast.WakeReceiver;
+import com.kaku.weac.common.WeacConstants;
+import com.kaku.weac.db.AlarmClockOperate;
 import com.kaku.weac.util.LogUtil;
+import com.kaku.weac.util.MyUtil;
+
+import java.util.List;
 
 public class DaemonService extends Service {
 
@@ -17,6 +32,14 @@ public class DaemonService extends Service {
      */
     private static final String LOG_TAG = "DaemonService";
 
+    /**
+     * 定时唤醒的时间间隔，5分钟
+     */
+    private final static int ALARM_INTERVAL = 5 * 60 * 1000;
+    private final static int WAKE_REQUEST_CODE = 6666;
+
+    private final static int GRAY_SERVICE_ID = -1001;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -24,13 +47,14 @@ public class DaemonService extends Service {
 
         Daemon.run(DaemonService.this, DaemonService.class, Daemon.INTERVAL_ONE_MINUTE);
 
-        Intent grayIntent = new Intent(getApplicationContext(), GrayService.class);
-        startService(grayIntent);
+        startTimeTask();
 
         // Notification notification = new Notification();
         // startForeground(-1, notification);
+    }
 
-/*        new Thread(new Runnable() {
+    private void startTimeTask() {
+        new Thread(new Runnable() {
             @Override
             public void run() {
                 List<AlarmClock> list = AlarmClockOperate.getInstance().loadAlarmClocks();
@@ -54,7 +78,7 @@ public class DaemonService extends Service {
                     }
                 }
             }
-        }).start();*/
+        }).start();
     }
 
     @Override
@@ -71,8 +95,60 @@ public class DaemonService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-//        LogUtil.d(LOG_TAG, "onStartCommand");
+        LogUtil.d(LOG_TAG, "onStartCommand");
+
+        grayGuard();
+
         flags = START_STICKY;
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void grayGuard() {
+        if (Build.VERSION.SDK_INT < 18) {
+            startForeground(GRAY_SERVICE_ID, new Notification());//API < 18 ，此方法能有效隐藏Notification上的图标
+        } else {
+            Intent innerIntent = new Intent(this, DaemonInnerService.class);
+            startService(innerIntent);
+            startForeground(GRAY_SERVICE_ID, new Notification());
+        }
+
+        //发送唤醒广播来促使挂掉的UI进程重新启动起来
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent alarmIntent = new Intent();
+        alarmIntent.setAction(WakeReceiver.GRAY_WAKE_ACTION);
+        PendingIntent operation = PendingIntent.getBroadcast(this, WAKE_REQUEST_CODE, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), ALARM_INTERVAL, operation);
+    }
+
+    /**
+     * 给 API >= 18 的平台上用的灰色保活手段
+     */
+    public static class DaemonInnerService extends Service {
+
+        @Override
+        public void onCreate() {
+            LogUtil.i(LOG_TAG, "InnerService -> onCreate");
+            super.onCreate();
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            LogUtil.i(LOG_TAG, "InnerService -> onStartCommand");
+            startForeground(GRAY_SERVICE_ID, new Notification());
+            //stopForeground(true);
+            stopSelf();
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            throw new UnsupportedOperationException("Not yet implemented");
+        }
+
+        @Override
+        public void onDestroy() {
+            LogUtil.i(LOG_TAG, "InnerService -> onDestroy");
+            super.onDestroy();
+        }
     }
 }
